de2e301ccb9333bc4df397772f7291bcdec0409d
Theo Jacquemin <tjacqu01@etudiant.univ-lr.fr>
2023-03-22 22:24:22 +0100

    Optimization of the code

--- Level2/PFCLS.py
+++ Level2/PFCLS.py
@@ -2,7 +2,43 @@ import random
 import os
 
 ## Differents options pour le jeu
-choix_options = ["Pierre", "Feuille", "Ciseau", "Lezard", "Spock"]
+choix_options = {
+    "Pierre": {
+        "Lezard": "C'est gagne !",
+        "Ciseaux": "C'est gagne !",
+        "Spock": "C'est perdu !",
+        "Feuille": "C'est perdu !",
+        "Pierre": "Match nul !"
+    },
+    "Feuille": {
+        "Pierre": "C'est gagne !",
+        "Spock": "C'est gagne !",
+        "Ciseaux": "C'est perdu !",
+        "Lezard": "C'est perdu !",
+        "Feuille": "Match nul !"
+    },
+    "Ciseaux": {
+        "Feuille": "C'est gagne !",
+        "Lezard": "C'est gagne !",
+        "Spock": "C'est perdu !",
+        "Pierre": "C'est perdu !",
+        "Ciseaux": "Match nul !"
+    },
+    "Lezard": {
+        "Spock": "C'est gagne !",
+        "Feuille": "C'est gagne !",
+        "Ciseaux": "C'est perdu !",
+        "Pierre": "C'est perdu !",
+        "Lezard": "Match nul !"
+    },
+    "Spock": {
+        "Pierre": "C'est gagne !",
+        "Ciseaux": "C'est gagne !",
+        "Lezard": "C'est perdu !",
+        "Feuille": "C'est perdu !",
+        "Spock": "Match nul !"
+    }
+}
 
 jouer = "Oui"
 
@@ -11,13 +47,13 @@ while jouer == "Oui":
     os.system('cls' if os.name == 'nt' else 'clear')
 
     ## Choix du joueur
-    choix_joueur = input("Pierre, Feuille, Ciseau, Lezard ou Spock ? ")
+    choix_joueur = input("Pierre, Feuille, Ciseaux, Lezard ou Spock ? ")
     while choix_joueur not in choix_options:
         choix_joueur = input("Pierre, Feuille, Ciseau, Lezard ou Spock ? ")   
 
 
     ## Choix de l'ia
-    choix_IA = choix_options[random.randint(0,4)]
+    choix_IA = list(choix_options.keys())[random.randint(0,4)]
 
 
     ## Affichage des choix
@@ -25,41 +61,7 @@ while jouer == "Oui":
     print ("Choix de l'IA : " + choix_IA)
 
     ## Gestion de match
-    if choix_joueur == "Pierre" :
-        if choix_IA == "Lezard" or choix_IA == "Ciseau" :
-            print ("Vous avez gagne !")
-        elif choix_IA == "Spock" or choix_IA == "Feuille" :
-            print("Vous avez perdu !")
-        else :
-            print ("Match nul !")
-    elif choix_joueur == "Lezard" :
-        if choix_IA == "Spock" or choix_IA == "Feuille" :
-            print ("Vous avez gagne !")
-        elif choix_IA == "Pierre" or choix_IA == "Ciseau" :
-            print("Vous avez perdu !")
-        else :
-            print ("Match nul !")
-    elif choix_joueur == "Spock" :
-        if choix_IA == "Ciseau" or choix_IA == "Pierre" :
-            print ("Vous avez gagne !")
-        elif choix_IA == "Lezard" or choix_IA == "Feuille" :
-            print("Vous avez perdu !")
-        else :
-            print ("Match nul !")
-    elif choix_joueur == "Ciseau" :
-        if choix_IA == "Feuile" or choix_IA == "Lezard" :
-            print ("Vous avez gagne !")
-        elif choix_IA == "Spock" or choix_IA == "Pierre" :
-            print("Vous avez perdu !")
-        else :
-            print ("Match nul !")
-    else :
-        if choix_IA == "Pierre" or choix_IA == "Spock" :
-            print ("Vous avez gagne !")
-        elif choix_IA == "Ciseau" or choix_IA == "Lezard" :
-            print("Vous avez perdu !")
-        else :
-            print ("Match nul !")
+    print(choix_options[choix_joueur][choix_IA])
 
     jouer = input("Voulez-vous rejouer (Oui ou Non) ? ")
     while jouer != "Oui" and jouer != "Non" :

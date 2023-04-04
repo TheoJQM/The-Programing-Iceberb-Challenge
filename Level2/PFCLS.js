e6cf5073d0354a1d89859a1cd2e2f9b2c687ae47
Theo Jacquemin <tjacqu01@etudiant.univ-lr.fr>
2023-04-04 21:21:04 +0200

    js version fully fonctionnal

--- Level2/PFCLS.js
+++ Level2/PFCLS.js
@@ -1,5 +1,3 @@
-
-
 // Differents options pour le jeu
 let choix_options = {
     "Pierre": { "Lezard": "C'est gagne !", "Ciseaux": "C'est gagne !", "Spock": "C'est perdu !", "Feuille": "C'est perdu !", "Pierre": "Match nul !"},
@@ -9,33 +7,49 @@ let choix_options = {
     "Spock": { "Pierre": "C'est gagne !", "Ciseaux": "C'est gagne !", "Lezard": "C'est perdu !", "Feuille": "C'est perdu !", "Spock": "Match nul !" }
 };
 
-// Varible pour rejouer
-let jouer = "oui";
+// Variables pour l'input
+const readline = require('readline');
 
-// Fonction pour que le joueur ne choississe que l'un des cinq choix proposés
-function choisir() {
-    readline.question('Pierre, Feuille, Ciseaux, Lezard ou Spock ?', (entree) => {
-        if(entree !== 'Pierre' && entree !== 'Feuille' && entree !== 'Ciseaux' && entree !== 'Lezard' && entree !== 'Spock') {
-            choisir();
-        } else {
-            choix_joueur = entree;
-            readline.close();
-        }
-    });
-}
-
-
-// Recupere le choix du joueur parmis les choix disponibles
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-let choix_joueur ="";
-choisir();
+// Le joueur choisit
+let choix_joueur = "";
 
+function jouer() {
+    // Recupere le choix de l'IA parmis les choix disponibles
+    choix_IA = Object.keys(choix_options).at(Math.floor(Math.random()*5));
+    console.log(choix_IA);
+    console.log(choix_joueur);
 
+    console.log(choix_options[choix_joueur][choix_IA]);
+    rejouer();
+}
 
-// Recupere le choix de l'IA parmis les choix disponibles
-choix_IA = Object.keys(choix_options).at(Math.floor(Math.random()*5));
-console.log(choix_IA);
+function readInput() {
+    rl.question('Pierre, Feuille, Ciseaux, Lezard ou Spock ? ', (input) => {
+        if (Object.keys(choix_options).includes(input)) {
+            choix_joueur = input;
+            jouer();
+        } else {
+            readInput();
+        }
+  });
+}
+
+function rejouer() {
+    rl.question('Voulez vous rejouer ? (Oui ou Non) ', (input) => {
+        if (input === "Oui") {
+            console.clear();
+            readInput();
+        } else if (input === "Non") {
+            rl.close();
+        } else {
+            rejouer();
+        }
+  });
+}
+
+readInput();
